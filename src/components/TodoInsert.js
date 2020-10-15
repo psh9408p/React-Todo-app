5f46dfa7b2840c2a72f0eda1e4bc7aa293309737
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-10-15 15:52:12 +0900

    todo-app add function oninsert

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import './TodoInsert.scss';
 import { MdAdd } from 'react-icons/md';
 //react-icons.netlify.com 에서 원하는 아이콘을 사용할수있는 라이브러리
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  //form을 쓴이유는 enter 로도 이벤트를 발생시킬수있기때문
   return (
-    <div className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
-    </div>
+    </form>
   );
 };
 
